d6096e18085c2fbf779aee7c4fc3a8870aa79b1a
karakoc49 <haluk_karakoc@hotmail.com>
2025-01-27 11:53:40 +0300

    speed insights bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import EasterEgg from './components/EasterEgg'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
